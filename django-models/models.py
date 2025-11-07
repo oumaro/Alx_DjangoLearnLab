f9f76ceddb729c8bc1877ef919c7a7feecfce540
oumar <omaroutil@gmail.com>
2025-11-07 19:10:53 +0100

    Update models.py

--- django-models/models.py
+++ django-models/models.py
@@ -1,70 +1,35 @@
 from django.db import models
 
+# Author Model
 class Author(models.Model):
-    """
-    Represents an author who writes books.
-    """
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        ordering = ['name']
 
-
+# Book Model
 class Book(models.Model):
-    """
-    Represents a book written by an author.
-    Uses ForeignKey to establish a many-to-one relationship with Author.
-    """
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(
-        Author,
-        on_delete=models.CASCADE,
-        related_name='books'
-    )
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.title} by {self.author.name}"
-
-    class Meta:
-        ordering = ['title']
+        return self.title
 
 
+# Library Model
 class Library(models.Model):
-    """
-    Represents a library that contains multiple books.
-    Uses ManyToManyField to establish a many-to-many relationship with Book.
-    """
     name = models.CharField(max_length=200)
-    books = models.ManyToManyField(
-        Book,
-        related_name='libraries'
-    )
+    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        verbose_name_plural = "Libraries"
-        ordering = ['name']
 
-
+# Librarian Model
 class Librarian(models.Model):
-    """
-    Represents a librarian who manages a library.
-    Uses OneToOneField to establish a one-to-one relationship with Library.
-    """
     name = models.CharField(max_length=200)
-    library = models.OneToOneField(
-        Library,
-        on_delete=models.CASCADE,
-        related_name='librarian'
-    )
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name} - {self.library.name}"
-
-    class Meta:
-        ordering = ['name']
+        return self.name
